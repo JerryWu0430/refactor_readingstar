e4bfc37fad7bb2ef891633985a3e16fec5650349
EdizCinbas <oecinbas@gmail.com>
2025-06-09 12:30:28 +0100

    fixing build config

--- readingstar/public/electron.js
+++ readingstar/public/electron.js
@@ -1,20 +1,40 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const isDev = !app.isPackaged;
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
       contextIsolation: true,
+      enableRemoteModule: false,
+      nodeIntegration: false
     },
   });
 
-  const startURL = app.isPackaged
-    ? `file://${path.join(__dirname, '../build/index.html')}`
-    : 'http://localhost:3000';
+  const startURL = isDev
+    ? 'http://localhost:3000'
+    : `file://${path.join(__dirname, '../build/index.html')}`;
 
   win.loadURL(startURL);
+
+  // Open DevTools in development
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
